f957f4e6e49edc0c57c383fc9a39ddcf81052657
arzmat <mattia.arzilli.2002@gmail.com>
2025-07-29 12:36:13 +0200

    fix

--- arduino_api/api/views.py
+++ arduino_api/api/views.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
 from django.utils import timezone
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 from .models import LightEvent, LightStatus
 import json
